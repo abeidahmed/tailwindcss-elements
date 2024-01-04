658bc8dcd2e4ca49db66fbe6691d3717457bfd09
abeidahmed <abeidahmed92@gmail.com>
2024-01-04 19:43:32 +0400

    feat: emit `twc-switch:change` cancelable event
    This event fires before toggling the switch. If this event is prevented
    `event.preventDefault`, the switch's state will not change.

--- packages/core/src/elements/switch/index.test.ts
+++ packages/core/src/elements/switch/index.test.ts
@@ -2,6 +2,18 @@ import { expect, fixture, html } from '@open-wc/testing';
 import SwitchElement from './index';
 import Sinon from 'sinon';
 
+function assertSwitchOn(el: SwitchElement, trigger: HTMLElement) {
+  expect(el).to.have.attribute('checked');
+  expect(el).to.have.attribute('data-headlessui-state', 'checked');
+  expect(trigger).to.have.attribute('aria-checked', 'true');
+}
+
+function assertSwitchOff(el: SwitchElement, trigger: HTMLElement) {
+  expect(el).not.to.have.attribute('checked');
+  expect(el).to.have.attribute('data-headlessui-state', '');
+  expect(trigger).to.have.attribute('aria-checked', 'false');
+}
+
 describe('Switch', () => {
   it('sets the attributes', async () => {
     const el = await fixture<SwitchElement>(html`
@@ -10,13 +22,10 @@ describe('Switch', () => {
       </twc-switch>
     `);
 
-    expect(el).not.to.have.attribute('checked');
-
-    const trigger = el.querySelector('button');
+    const trigger = el.querySelector('button')!;
+    assertSwitchOff(el, trigger);
     expect(trigger).to.have.attribute('role', 'switch');
     expect(trigger).to.have.attribute('tabindex', '0');
-    expect(trigger).to.have.attribute('aria-checked', 'false');
-    expect(el).to.have.attribute('data-headlessui-state', '');
   });
 
   it('defaults to the checked state', async () => {
@@ -28,9 +37,8 @@ describe('Switch', () => {
 
     expect(el).to.have.attribute('checked');
 
-    const trigger = el.querySelector('button');
-    expect(trigger).to.have.attribute('aria-checked', 'true');
-    expect(el).to.have.attribute('data-headlessui-state', 'checked');
+    const trigger = el.querySelector('button')!;
+    assertSwitchOn(el, trigger);
   });
 
   it('toggles the checked state', async () => {
@@ -39,21 +47,23 @@ describe('Switch', () => {
         <button type="button" data-target="twc-switch.trigger">Switch</button>
       </twc-switch>
     `);
+    const changeHandler = Sinon.spy();
+    el.addEventListener(`${el.identifier}:change`, changeHandler);
+
     const changedHandler = Sinon.spy();
     el.addEventListener(`${el.identifier}:changed`, changedHandler);
 
     const trigger = el.querySelector('button')!;
     trigger.click();
-    expect(el).to.have.attribute('checked');
-    expect(trigger).to.have.attribute('aria-checked', 'true');
-    expect(el).to.have.attribute('data-headlessui-state', 'checked');
+    assertSwitchOn(el, trigger);
     expect(changedHandler.calledOnce).to.be.true;
+    expect(changeHandler.calledOnce).to.be.true;
+    expect(changedHandler.calledAfter(changeHandler)).to.be.true;
 
     trigger.click();
-    expect(el).not.to.have.attribute('checked');
-    expect(trigger).to.have.attribute('aria-checked', 'false');
-    expect(el).to.have.attribute('data-headlessui-state', '');
+    assertSwitchOff(el, trigger);
     expect(changedHandler.calledTwice).to.be.true;
+    expect(changeHandler.calledTwice).to.be.true;
   });
 
   it('programmatically toggling the checked state', async () => {
@@ -62,20 +72,38 @@ describe('Switch', () => {
         <button type="button" data-target="twc-switch.trigger">Switch</button>
       </twc-switch>
     `);
+    const changeHandler = Sinon.spy();
+    el.addEventListener(`${el.identifier}:change`, changeHandler);
+
     const changedHandler = Sinon.spy();
     el.addEventListener(`${el.identifier}:changed`, changedHandler);
 
     el.checked = true;
 
     const trigger = el.querySelector('button')!;
-    expect(trigger).to.have.attribute('aria-checked', 'true');
-    expect(el).to.have.attribute('data-headlessui-state', 'checked');
+    assertSwitchOn(el, trigger);
+    expect(changeHandler.called).to.be.false;
     expect(changedHandler.called).to.be.false;
 
     el.checked = false;
-    expect(trigger).to.have.attribute('aria-checked', 'false');
-    expect(el).to.have.attribute('data-headlessui-state', '');
+    assertSwitchOff(el, trigger);
+    expect(changeHandler.called).to.be.false;
     expect(changedHandler.called).to.be.false;
+  });
+
+  it('toggling can be stopped by preventing the change event', async () => {
+    const el = await fixture<SwitchElement>(html`
+      <twc-switch>
+        <button type="button" data-target="twc-switch.trigger">Switch</button>
+      </twc-switch>
+    `);
+    el.addEventListener(`${el.identifier}:change`, (event) => {
+      event.preventDefault();
+    });
+
+    const trigger = el.querySelector('button')!;
+    trigger.click();
+    assertSwitchOff(el, trigger);
   });
 
   it('specified tabindex is not overridden', async () => {
