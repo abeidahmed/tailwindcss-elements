6d8c14ae723fc32ea4f4e1b77a29d935a1bc6dd1
Abeid Ahmed <abeidahmed92@gmail.com>
2024-05-25 13:32:03 +0530

    fix: element exports (#42)

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,13 +1,33 @@
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import { globbySync } from 'globby';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import copy from 'rollup-plugin-copy';
+
+const production = process.env.NODE_ENV === 'production';
+const exportPaths = ['./src/elements/**/!(*.test).ts', './src/hooks/**/!(*.test).ts'];
+
+function pathEntries() {
+  // Need to export these in package.json
+  return Object.fromEntries(
+    globbySync(exportPaths).map((file) => [
+      path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+      fileURLToPath(new URL(file, import.meta.url)),
+    ])
+  );
+}
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 export default [
   {
-    input: ['./src/index.ts'],
+    input: {
+      index: './src/index.ts',
+      ...pathEntries(),
+    },
     output: [
       {
         dir: 'dist',
@@ -16,6 +36,7 @@ export default [
         preserveModulesRoot: 'src',
       },
     ],
+    external: ['@ambiki/impulse'],
     plugins: [
       resolve(),
       typescript({ tsconfig: './tsconfig.build.json' }),
@@ -39,6 +60,7 @@ export default [
     plugins: [
       resolve({ browser: true }),
       typescript({ tsconfig: './tsconfig.build.json' }),
+      ...(production ? [terser()] : []),
       copy({
         targets: [
           { src: '../../README.md', dest: '.' },
